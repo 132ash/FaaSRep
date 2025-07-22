c73590654e7827597c06d9bcc51214c069269e85
132ash <132ash@sjtu.edu.cn>
2025-07-22 08:13:28 +0000

    beldi config modified

--- faas/config/config.py
+++ faas/config/config.py
@@ -1,6 +1,6 @@
 # basic settings
-COUCHDB_URL = 'http://faasnap:faasnap@192.168.162.132:5984'
-DYNAMODB_URL = 'http://192.168.162.132:4567'
+COUCHDB_URL = 'http://faasnap:faasnap@10.2.27.24:5984'
+DYNAMODB_URL = 'http://10.2.27.24:4567'
 DYNAMODB_KEY_ID = 'FAASNAPDYNAMODB'
 DYNAMODB_ACCESS_KEY = 'FAASNAPDYNAMODBKEY'
 DYNAMODB_AREA = 'us-west-2'
@@ -8,7 +8,7 @@ REDIS_HOST = '127.0.0.1' # it serves to connect with the local redis, so it shou
 REDIS_PORT = 6379 # it follows the same configuration as created redis by docker (e.g., -p 6379:6379)
 SHADOWTABLE_DB = 0
 CACHE_DB = 1
-GATEWAY_ADDR = '192.168.162.132:8000' # need to update as your private_ip
+GATEWAY_ADDR = '10.2.27.24:8000' # need to update as your private_ip
 
 
 # workflow setting
@@ -36,13 +36,6 @@ VALIDATORS_PER_POOL = 2
 BATCH_SIZE = 1
 BATCH_INTERVAL = 0.005
 
-# repair setting
-RUNNING = 1
-REPAIRED = 2
-ABORTED = 3
-
-# FaaSTCC setting
-DEFAULT_SNAPSHOT_INTERVAL = []
 
 
 
