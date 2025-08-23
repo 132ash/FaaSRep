036699fd4b7a2e2c5d87f5cbdc320d122b619ed4
132ash <132ash@sjtu.edu.cn>
2025-08-23 11:19:25 +0800

    parameters changed

--- config/config.py
+++ config/config.py
@@ -21,20 +21,20 @@ WORKERSP_PORT = '7500'
 # workflow setting
 WORKFLOW_YAML_ADDR = {
                     # 'textseq': f"{ROOT_DIR}/benchmark/textseq",
-                    # 'c2': f"{ROOT_DIR}/benchmark/micro_benchmark/c2",
-                    # 'c4': f"{ROOT_DIR}/benchmark/micro_benchmark/c4",
-                    # 'c8': f"{ROOT_DIR}/benchmark/micro_benchmark/c8",
-                    # 'c16': f"{ROOT_DIR}/benchmark/micro_benchmark/c16",
-                    # 'w2': f"{ROOT_DIR}/benchmark/micro_benchmark/w2",
-                    # 'w4': f"{ROOT_DIR}/benchmark/micro_benchmark/w4",
-                    # 'w8': f"{ROOT_DIR}/benchmark/micro_benchmark/w8",
-                    # 'w16': f"{ROOT_DIR}/benchmark/micro_benchmark/w16",
-                    'travel_reservation': f"{ROOT_DIR}/benchmark/travel_reservation",
-                    'banking_system': f"{ROOT_DIR}/benchmark/banking_system",   
-                    'social_network': f"{ROOT_DIR}/benchmark/social_network",  
+                    'c2': f"{ROOT_DIR}/benchmark/micro_benchmark/c2",
+                    'c4': f"{ROOT_DIR}/benchmark/micro_benchmark/c4",
+                    'c8': f"{ROOT_DIR}/benchmark/micro_benchmark/c8",
+                    'c16': f"{ROOT_DIR}/benchmark/micro_benchmark/c16",
+                    'w2': f"{ROOT_DIR}/benchmark/micro_benchmark/w2",
+                    'w4': f"{ROOT_DIR}/benchmark/micro_benchmark/w4",
+                    'w8': f"{ROOT_DIR}/benchmark/micro_benchmark/w8",
+                    'w16': f"{ROOT_DIR}/benchmark/micro_benchmark/w16",
+                    # 'travel_reservation': f"{ROOT_DIR}/benchmark/travel_reservation",
+                    # 'banking_system': f"{ROOT_DIR}/benchmark/banking_system",   
+                    # 'social_network': f"{ROOT_DIR}/benchmark/social_network",  
                       }
 
-DEFAULT_CONTAINER_NUM = 16
+DEFAULT_CONTAINER_NUM = 32
 CLEAR_MEM = True
 
 # microbenchmark configuration
